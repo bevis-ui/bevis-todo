a28eb4c2e4989ce6acc79a63a5e8de810ea31240
pahaz <pahaz.blinov@gmail.com>
2015-01-10 06:14:48 +0500

    up blocks and pages form stub

--- pages/index-page/index-page.page.js
+++ pages/index-page/index-page.page.js
@@ -8,43 +8,30 @@ module.exports = function (pages) {
                 {url: options.assetsPath + '.css'}
             ],
             scripts: [
-                {url: [options.assetsPath, params.lang, 'js'].join('.')}
+                {url: options.assetsPath + '.' + params.lang + '.js'}
             ],
             body: [
                 {
                     block: 'layout',
-                    content: {
-                        block: 'todo-window'
-                    },
-                    aside: [
-                        {
-                            block: 'sidebar',
+                    aside: {
+                        block: 'sidebar',
 
-                            title: 'BEViS application',
-                            about: [
-                                {
-                                    text: 'Source',
-                                    url: 'https://github.com/bevis-ui/bevis-todo'
-                                }
-                            ],
-                            quote: {
-                                author: 'BEViS',
-                                text: 'BEViS - это не только соглашения об именовании тегов и CSS-классов.<br><br>' +
-                                    'Это ещё и готовые инструменты, ' +
-                                    'с которыми сверстать полноценный динамический сайт может даже подросток.'
+                        title: 'Привет, BEViS!',
+                        resources: [
+                            {
+                                text: 'Репозиторий',
+                                url: 'https://github.com/bevis-ui/'
                             },
-                            resources: [
-                                {
-                                    text: 'Documentation',
-                                    url: 'https://github.com/bevis-ui/docs'
-                                },
-                                {
-                                    text: 'BEViS on Github',
-                                    url: 'https://github.com/bevis-ui'
-                                }
-                            ]
-                        }
-                    ]
+                            {
+                                text: 'Учебник для новичков',
+                                url: 'https://github.com/bevis-ui/docs/blob/master/manual-for-beginner.md'
+                            },
+                            {
+                                text: 'Учебник для старичков',
+                                url: 'https://github.com/bevis-ui/docs/blob/master/manual-for-master.md'
+                            },
+                        ]
+                    }
                 }
             ]
         };
